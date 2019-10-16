616c09a537307db684ab6af03eff19a826f7cdcb
Rui Chen <chenrui333@gmail.com>
2019-10-15 22:45:40 -0400

    nushell 0.4.0
     - Fix test and make it more robust
     - Use rust beta instead of nightly

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -1,9 +1,8 @@
 class Nushell < Formula
   desc "Modern shell for the GitHub era"
   homepage "https://www.nushell.sh"
-  url "https://github.com/nushell/nushell/archive/0.2.0.tar.gz"
-  sha256 "5bce8cdb33a6580ff15214322bc66945c0b4d93375056865ad30e0415fece3de"
-  revision 1
+  url "https://github.com/nushell/nushell/archive/0.4.0.tar.gz"
+  sha256 "7111d3067db3dabc6137e8a441670de0fae63deae522d1675fc77402948f2c67"
   head "https://github.com/nushell/nushell.git"
 
   bottle do
@@ -17,21 +16,25 @@ class Nushell < Formula
   depends_on "openssl@1.1"
 
   # Nu requires features from Rust 1.39 to build, so we can't use Homebrew's
-  # Rust; picking a known-good Rust nightly release to use instead.
-  resource "rust-nightly" do
-    url "https://static.rust-lang.org/dist/2019-08-24/rust-nightly-x86_64-apple-darwin.tar.xz"
-    sha256 "104ddea51b758f4962960097e9e0f3cabf2c671ec3148bc745344431bb93605d"
+  # Rust; picking a known-good Rust beta release to use instead.
+  resource "rust-beta" do
+    url "https://static.rust-lang.org/dist/2019-10-16/rust-beta-x86_64-apple-darwin.tar.xz"
+    sha256 "205e9483d6e3fc9ba967613b3d9dc244ec3aeb1f5cf26bfb28c03560c0850d22"
   end
 
   def install
-    resource("rust-nightly").stage do
-      system "./install.sh", "--prefix=#{buildpath}/rust-nightly"
-      ENV.prepend_path "PATH", "#{buildpath}/rust-nightly/bin"
+    resource("rust-beta").stage do
+      system "./install.sh", "--prefix=#{buildpath}/rust-beta"
+      ENV.prepend_path "PATH", "#{buildpath}/rust-beta/bin"
     end
     system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
-    assert_equal "#{Dir.pwd}> 2\n#{Dir.pwd}> CTRL-D\n", pipe_output("#{bin}/nu", 'echo \'{"foo":1, "bar":2}\' | from-json | get bar | echo $it')
+    (testpath/"get-json.nu").write <<~EOS
+      echo '{"foo": 1, "bar": "this very specific text"}' | from-json | get bar | echo $it
+      exit
+    EOS
+    assert_match " this very specific text\n", shell_output("#{bin}/nu < get-json.nu")
   end
 end
