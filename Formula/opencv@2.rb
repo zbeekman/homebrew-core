45eb4e0f5dcf85e2182848519a3fc6d0f740223f
Izaak Beekman <zbeekman@gmail.com>
2020-02-17 13:07:59 -0500

    opencv@2: revision bump for openblas built with clang

--- Formula/opencv@2.rb
+++ Formula/opencv@2.rb
@@ -3,7 +3,7 @@ class OpencvAT2 < Formula
   homepage "https://opencv.org/"
   url "https://github.com/opencv/opencv/archive/2.4.13.7.tar.gz"
   sha256 "192d903588ae2cdceab3d7dc5a5636b023132c8369f184ca89ccec0312ae33d0"
-  revision 7
+  revision 8
 
   bottle do
     sha256 "31719e8af1404aca919073f25576ff2dceb880aa0fc91d863f7a73ac0073f598" => :catalina
